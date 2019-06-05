40f91f023e0958793a173ac8479265e182f12520
Abhinav Mishra <abhinavmish96@gmail.com>
2019-06-05 19:20:35 +0530

    <chap 1>

--- 1_NodeJS_Up_and_Running/service.js
+++ 1_NodeJS_Up_and_Running/service.js
@@ -2,19 +2,19 @@ var http = require('http');
 var fs = require('fs');
 
 // write out numbers
-function writeumbers(res){
+function writeNumbers(res){
     
     var counter = 0;
 
     //increment global write to client
-    for(var i= 0; i<100; i++){
+    for(var i = 0; i<100; i++){
         counter++;
         res.write(counter.toString() + '\n');
     }
 }
 
 // create http server
-http.createServer((req,res)=>{
+http.createServer((req,res) => {
 
     var query = require('url').parse(req.url).query;
     var app = require('querystring').parse(query).file + ".txt";
@@ -25,18 +25,16 @@ http.createServer((req,res)=>{
     // write out numbers
     writeNumbers(res);
     
-   // open and read in helloWorld.js
+   // time to open and read contents
    setTimeout(function(){
        
-        console.log('opening' + app);
+        console.log('opening ' + app);
        // open and read in helloWorld.js
         fs.readFile(app, 'utf-8', function(err, data){
-        
-            // content header
-            res.writeHead(200, {'content-type': 'text/plain'});
     
-            if(err) res.write('Could not find or open file for reading\n');
-            else
+            if(err) 
+            res.write('Could not find or open file for reading\n');
+            else 
             res.write(data);
             
             res.end();
